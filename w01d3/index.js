c2fed7f1c8b62ac80732fa64d997e49af9b90564
Pedro A. González <pgonzalez@post.com>
2022-10-19 11:33:33 -0500

    Add notes p2 during lecture

--- w01d3/index.js
+++ w01d3/index.js
@@ -58,3 +58,53 @@ const reassingCar = function(car) {
 
 reassingCar(myCar);
 console.log('outside reassign', myCar);
+
+// FUNCTIONS INSIDE OBJECTS
+// object {
+//   key: value,
+//   methods()
+// }
+
+const cat = {
+  name: 'Garfield',
+  color: 'orange',
+  sayHi: function() {
+    console.log('Meooooowwww!! 🐈');
+  }
+};
+
+cat.sayHi();
+
+const person = {
+  firstName: 'David',
+  lastName: 'Figueroa',
+  sayFullName: function() {
+    console.log(`My full name is: ${this.firstName} ${this.lastName}`);
+  }
+};
+
+person.sayFullName();
+
+// OBJECT PROPERTIES ITERATION
+const fruit = {
+  name: 'Apple',
+  color: 'green',
+  emoji: '🍏'
+};
+
+// FOR OF
+// Convert the keys of the object into an array
+const fruitKeys = Object.keys(fruit);
+// We iterate through those keys as any other array with a for of loop
+for (var key of fruitKeys) {
+  console.log(fruit[key]);
+}
+
+// FOR IN
+for (var key in fruit) {
+  console.log(fruit[key]);
+}
+
+// IN JS EVERYTHING IS AN OBJECT!
+const myString = 'hello!';
+console.log(myString.__proto__);
